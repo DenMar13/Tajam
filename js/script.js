a3ea8a494184a702c301517dd01899ad0b61eb2a
DenMar13 <81777590+DenMar13@users.noreply.github.com>
2021-08-20 18:13:10 +0300

    Delete script.js

--- js/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('.header-burger').click(function(event) {
-        $('.header-burger, nav').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
- });
